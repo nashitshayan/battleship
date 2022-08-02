6f46820bf7119a08bd485accc8a115cd3b4aa945
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-02 16:00:39 +0530

    Test: hit function

--- shipFactory/shipFactory.js
+++ shipFactory/shipFactory.js
@@ -1,4 +1,9 @@
 export const shipFactory = (length) => {
 	const body = new Array(length);
-	return { body };
+	const hit = (x) => {
+		if (x >= body.length) throw new Error();
+		body[x] = 'hit';
+		return body;
+	};
+	return { body, hit };
 };
--- shipFactory/shipFactory.test.js
+++ shipFactory/shipFactory.test.js
@@ -13,4 +13,10 @@ describe('Ship Factory', () => {
 	test("ship should take a 'length' and return a 'body' array of that length", () => {
 		expect(shipFactory(4).body.length).toEqual(4);
 	});
+	test("ship should have a 'hit' function that takes a value and marks that postion as 'hit ", () => {
+		expect(shipFactory(4).hit(2)[2]).toEqual('hit');
+	});
+	test("'hit' function shouldn't be able to extend the ship body", () => {
+		expect(() => shipFactory(4).hit(5)[5]).toThrow();
+	});
 });
