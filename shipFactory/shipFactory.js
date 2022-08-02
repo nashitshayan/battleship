c882f18e1ad7c7a726f45f0f50df853e14ef9691
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-02 16:25:02 +0530

    Test: isSunk function

--- shipFactory/shipFactory.js
+++ shipFactory/shipFactory.js
@@ -1,9 +1,10 @@
 export const shipFactory = (length) => {
-	const body = new Array(length);
+	const body = new Array(length).fill(0);
 	const hit = (x) => {
 		if (x >= body.length) throw new Error();
 		body[x] = 'hit';
 		return body;
 	};
-	return { body, hit };
+	const isSunk = () => body.every((pos) => pos === 'hit');
+	return { body, hit, isSunk };
 };
--- shipFactory/shipFactory.test.js
+++ shipFactory/shipFactory.test.js
@@ -19,4 +19,15 @@ describe('Ship Factory', () => {
 	test("'hit' function shouldn't be able to extend the ship body", () => {
 		expect(() => shipFactory(4).hit(5)[5]).toThrow();
 	});
+	test("ship should have a 'isSunk' function that checks whether all positions have been 'hit'", () => {
+		expect(shipFactory(4).isSunk()).toBe(false);
+	});
+	test("ship should 'sink' when all position are 'hit", () => {
+		const ship = shipFactory(4);
+		ship.hit(0);
+		ship.hit(1);
+		ship.hit(2);
+		ship.hit(3);
+		expect(ship.isSunk()).toBe(true);
+	});
 });
