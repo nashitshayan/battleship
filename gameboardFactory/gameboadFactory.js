016525892eb9d918e2cc06efb8899a3e2922536d
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-03 10:16:12 +0530

    Test: gameboard should be an object

--- gameboardFactory/gameboadFactory.js
+++ gameboardFactory/gameboadFactory.js
@@ -1,3 +1,3 @@
 export const gameboardFactory = () => {
-	return;
+	return {};
 };
--- gameboardFactory/gameboadFactory.test.js
+++ gameboardFactory/gameboadFactory.test.js
@@ -4,4 +4,7 @@ describe('Gameboard Factory', () => {
 	test("Gameboard shouldn't be null", () => {
 		expect(gameboardFactory()).not.toBeNull();
 	});
+	test('Gameboard should be an object', () => {
+		expect(typeof gameboardFactory()).toEqual('object');
+	});
 });
