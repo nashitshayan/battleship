34a39a7fee1bda29988ecb91e649ad9f8abe3146
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-04 15:08:21 +0530

    Test: initial gameboard

--- gameboardFactory/gameboadFactory.js
+++ gameboardFactory/gameboadFactory.js
@@ -1,3 +1,161 @@
 export const gameboardFactory = () => {
-	return {};
+	const rows = 10;
+	const cols = 10;
+	const board = [];
+
+	//create a 2-d array filled with 0's. This will be our initial gameboard
+	for (let i = 0; i < rows; i++) {
+		board[i] = [];
+		for (let j = 0; j < cols; j++) {
+			board[i].push(0);
+		}
+	}
+
+	const getBoard = () => board;
+
+	return { getBoard };
 };
+
+/**
+Gameboard factory:
+After reading Bender’s Connect Four article, I got some ideas. The Gameboard will be a 2-d array, and it will call the ship factory and place the ships on the board. 
+- So the gameboard factory can start off as something like this :
+- (I realised later that I wrote the code here before the test lol)
+```
+const gameboardFactory= (){
+	const rows= 10;
+	const cols= 10;
+	const board= []
+
+	// 2-d array to represent the board. Initially, the board will be empty (without ships), so each cell will contain 0
+
+	for(let i=0; i<rows; i++){
+		board[i]=[];
+		for(let j=0; j<cols; j++){
+			board[i].push(0);
+		}
+	}
+	
+}
+```
+- Gameboard should return an object with 
+	- a function to get the board (DONE)
+	- a function to print the board on console
+	- a function to place ships on the board
+	- a function recieveAttack that takes a pair of coords, determines whether or not the attack hit a ship, then sends the hit() function to the correct ship, or records the coordinates of the missed shot.
+	- a function to report whether all ships have sunk or not 
+
+
+*“1. Gameboards should be able to place ships at specific coordinates by calling the ship factory function.”*
+
+The ship factory instruction says : *“1. Your ‘ships’ will be objects that include their length, where they’ve been hit and whether or not they’ve been sunk.”* 
+
+So the assumption is that the ship factory doesn’t do anything else. Well then what about the ship types? Are they specified in the ship factory or is it the gameboard that specifies the ship type and the coordinates where to place them?
+	- I think they should be passed to the ship factory from gameboard. 
+	- This is because I think the player should get to choose where a particular ship is placed. 
+		- for eg: place 'patrol boat' at (5,8)
+	- So then these two things - coordinates, ship type should come from the gameController, meaning the player.
+	- gameController -> placeShip -> ship factory
+	- This means that this operation of placing ships would have to be done one at a time. The placeShip function will get called for each shipType. 
+	- so the shipType array should be on gameController. We iterate through each ship and ask the player where to place it. 
+	- Player enters the co-ords and we call placeShip to add the ship and then we display the updated gameboard. Then repeat for each ship.
+	
+
+So, how will the placeShip function work? 
+
+- The coordinates will basically be the row and col number.
+- The player can choose the coordinates for each ship type.
+- * the placeShip function takes in the coordinates and the ship type and places the ship on the board. 
+	- how will it place the ship on the board? Ship factory returns an object
+	- we also want the ship to occupy the cells on the board based on its length
+	- SHIP TYPES - Carrier(5), Battleship(4), Destroyer(3), Submarine(3), Patrol Boat(2)
+	- also, some ships will be placed horizontally and others vertically. Gotta take care about that too. Hmm
+		- maybe shipTypes can be an array of OBJECTS
+		- eg: shipTypes= [{name: 'battleship', axis: 'horizontal'}, ...]
+		- maybe based on this 'axis' property, placeShip can choose to place them horizontally or vertically
+
+	- okay so I have a 10x10 0-filled grid. The ship factory right now returns a ship body which is an array.
+	- How do I 'place' this ship on the board?
+	- What I want is that the board gets marked with 1's wherever there is a ship
+		- so it should become something like this 
+		- [
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 0, 0, 1, 1, 0],
+			[1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[1, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+		]
+		- So first, I'll have to change the ship body array to be an array of 1's initially. And on every hit, replace with an "x", when the whole array gets filled with 'x's then the ship is sunk
+		- I'll also need to do something so that based on the shipType from the param, a ship body of a particular length is created. 
+			- maybe have an object that that maps shipTypes to their length.
+			- eg: const shipLength= {
+				'battleship': 4,
+				//etc
+			}
+			- then-> const shipBody= new Array(shipLength[shipType]).fill(1)
+		
+		- As for placing the ship on the board, I can iterate through the returned body array and mark the spots on the board with the values (1's). But then how to deal with hits and updating the board?
+		- the ship factory should take in Type and Length.
+
+
+	TODO:
+	-(DONE) test if gameboard factory returns a method that will give us an empty 10x10 board (filled with zeros)
+		- gameboard should create this 0 filled 2-d grid and have a fn to get the board.
+		- how to test it tho?
+			- the return value will be a 2-d array filled with zeroes. Ofc another 2-d array filled with 0's will not be equal to this because js compares non-primitive values by reference. 
+			- lol, found a nice hack on stack overflow
+				- JSON.stringyfy the arrays and then compare!
+	- test the placeShip function
+		- how, lol?
+		- it's an incoming 'command' message, it changes the internal state but does not return anything back.
+		- maybe we call this function inside the test with the params and then expect the board to be filled with the ship
+		- eg: 
+			test('placeShip should place a ship on the gameboard', () => {
+				let updatedGridStringyfied = JSON.stringify([
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				]);
+				
+				gameboardFactory().placeShip({name: 'carrier', axis: 'vertical'}, 5,9);
+
+				expect(JSON.stringify(gameboardFactory().getBoard())).toEqual(
+				stringyfiedInitialGrid,
+				);
+			});
+ */
+
+/**
+ Hey, I'm on the battleship project and having some trouble figuring out how to 'place' the ships on the board.
+Here's what I've come up with so far, please let me know I'm heading in the wrong direction :
+```md
+-- board is a 10x10 2d array, initially filled with 0's
+-- the placeShip function takes in the shipType and co-ords (row and col number), calls shipFactory, places the ship on the board.
+  eg: This is how it should play out : Place a 'battleship (length 4)' at (4,5)
+      --[
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	]
+-- The problem is that the ship factory is supposed to create the ship array right? How then, should this array be placed on the board? 
+``` 
+ */
--- gameboardFactory/gameboadFactory.test.js
+++ gameboardFactory/gameboadFactory.test.js
@@ -7,4 +7,21 @@ describe('Gameboard Factory', () => {
 	test('Gameboard should be an object', () => {
 		expect(typeof gameboardFactory()).toEqual('object');
 	});
+	test('Gameboard should expose a function that gives us the initial 10x10 0-filled grid', () => {
+		let stringyfiedInitialGrid = JSON.stringify([
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		]);
+		expect(JSON.stringify(gameboardFactory().getBoard())).toEqual(
+			stringyfiedInitialGrid,
+		);
+	});
 });
