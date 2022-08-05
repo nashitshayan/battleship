803820dd983002240df22312a77d28650366e125
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-05 10:21:27 +0530

    Refactor ship hit

--- shipFactory/shipFactory.js
+++ shipFactory/shipFactory.js
@@ -2,9 +2,9 @@ export const shipFactory = (length) => {
 	const body = new Array(length).fill(0);
 	const hit = (x) => {
 		if (x >= body.length) throw new Error();
-		body[x] = 'hit';
+		body[x] = 'x';
 		return body;
 	};
-	const isSunk = () => body.every((pos) => pos === 'hit');
+	const isSunk = () => body.every((pos) => pos === 'x');
 	return { body, hit, isSunk };
 };
