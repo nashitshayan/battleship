614c187befdede50a20e257e48482fc50ad8e82b
Nashit Shayan <shayan.nashit@gmail.com>
2022-08-07 10:15:05 +0530

    Change shipFactory

--- shipFactory/shipFactory.js
+++ shipFactory/shipFactory.js
@@ -1,12 +1,15 @@
-export const shipFactory = (length, name) => {
-	const ship = new Array(length).fill(1);
-	const getName = () => name;
-	const getShip = () => ship;
-	const hit = (x) => {
-		if (x >= ship.length) throw new Error();
-		ship[x] = 'x';
-		return ship;
-	};
-	const isSunk = () => ship.every((pos) => pos === 'x');
-	return { getShip, getName, hit, isSunk };
+// export const shipFactory = (length, name) => {
+// 	const ship = new Array(length).fill(1);
+// 	const getName = () => name;
+// 	const getShip = () => ship;
+// 	const hit = (x) => {
+// 		if (x >= ship.length) throw new Error();
+// 		ship[x] = 'x';
+// 		return ship;
+// 	};
+// 	const isSunk = () => ship.every((pos) => pos === 'x');
+// 	return { getShip, getName, hit, isSunk };
+// };
+export const shipFactory = (name) => {
+	return { isHit: false, name };
 };
--- shipFactory/shipFactory.test.js
+++ shipFactory/shipFactory.test.js
@@ -7,30 +7,38 @@ describe('Ship Factory', () => {
 	test('ship should be an object', () => {
 		expect(typeof shipFactory()).toEqual('object');
 	});
-	test("ship should have a 'ship' array", () => {
-		expect(Array.isArray(shipFactory().getShip())).toEqual(true);
+	test('ship should have an isHit property', () => {
+		const carrier = shipFactory('carrier');
+		expect(carrier.hasOwnProperty('isHit')).toEqual(true);
 	});
-	test("shipFactory should take a 'length' and return a 'ship' array of that length", () => {
-		expect(shipFactory(4).getShip().length).toEqual(4);
+	test('ship should have a name', () => {
+		const carrier = shipFactory('carrier');
+		expect(carrier.name).toEqual('carrier');
 	});
-	test("shipFactory should take a 'name' and return an object with the name property", () => {
-		expect(shipFactory(4, 'battleship').getName()).toEqual('battleship');
-	});
-	test("ship should have a 'hit' function that takes a value and marks that postion as 'x' ", () => {
-		expect(shipFactory(4).hit(2)[2]).toEqual('x');
-	});
-	test("'hit' function shouldn't be able to extend the ship body", () => {
-		expect(() => shipFactory(4).hit(5)[5]).toThrow();
-	});
-	test("ship should have a 'isSunk' function that checks whether all positions have been 'hit'", () => {
-		expect(shipFactory(4).isSunk()).toBe(false);
-	});
-	test("ship should 'sink' when all position are 'hit", () => {
-		const ship = shipFactory(4);
-		ship.hit(0);
-		ship.hit(1);
-		ship.hit(2);
-		ship.hit(3);
-		expect(ship.isSunk()).toBe(true);
-	});
+	// test("ship should have a 'ship' array", () => {
+	// 	expect(Array.isArray(shipFactory().getShip())).toEqual(true);
+	// });
+	// test("shipFactory should take a 'length' and return a 'ship' array of that length", () => {
+	// 	expect(shipFactory(4).getShip().length).toEqual(4);
+	// });
+	// test("shipFactory should take a 'name' and return an object with the name property", () => {
+	// 	expect(shipFactory(4, 'battleship').getName()).toEqual('battleship');
+	// });
+	// test("ship should have a 'hit' function that takes a value and marks that postion as 'x' ", () => {
+	// 	expect(shipFactory(4).hit(2)[2]).toEqual('x');
+	// });
+	// test("'hit' function shouldn't be able to extend the ship body", () => {
+	// 	expect(() => shipFactory(4).hit(5)[5]).toThrow();
+	// });
+	// test("ship should have a 'isSunk' function that checks whether all positions have been 'hit'", () => {
+	// 	expect(shipFactory(4).isSunk()).toBe(false);
+	// });
+	// test("ship should 'sink' when all position are 'hit", () => {
+	// 	const ship = shipFactory(4);
+	// 	ship.hit(0);
+	// 	ship.hit(1);
+	// 	ship.hit(2);
+	// 	ship.hit(3);
+	// 	expect(ship.isSunk()).toBe(true);
+	// });
 });
